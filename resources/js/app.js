bdc8210045b78fedb017988852f313b2922c273f
vchandra22 <vincentchaandra@gmail.com>
2024-11-28 16:23:05 +0700

    feat: add the speedinsight by vercel

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,14 @@ import './bootstrap';
 import 'flowbite';
 import './aos';
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+// Initialize and use Speed Insights
+const speed = new SpeedInsights({
+  url: window.location.href, // Current page URL
+});
+
+speed.track();
+
 import { inject } from '@vercel/analytics';
 inject();
