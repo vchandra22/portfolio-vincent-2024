86e3173263ba589d192dff654fd2613576a22fb6
vchandra22 <vincentchaandra@gmail.com>
2024-11-28 16:28:43 +0700

    fix: remove the speedinsight by vercel

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,14 +2,5 @@ import './bootstrap';
 import 'flowbite';
 import './aos';
 
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
-// Initialize and use Speed Insights
-const speed = new SpeedInsights({
-  url: window.location.href, // Current page URL
-});
-
-speed.track();
-
 import { inject } from '@vercel/analytics';
 inject();
